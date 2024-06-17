72ffd9eff88c670731aa89e655282c1c81c72252
Philix27 <philixbob@gmail.com>
2024-06-17 05:51:09 +0100

    added packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Report;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Report> {
+    setup()?;
+    println!("Hello from a (so far completely unnecessary) async runtime");
+    Ok(())
+}
+
+fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+    color_eyre::install()?;
+    Ok(())
 }
